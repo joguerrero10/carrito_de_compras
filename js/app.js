6ec99293808043282b14729189b63e1665e4d5de
Joel Guerrero <joelg1014@hotmail.com>
2025-11-20 10:44:34 -0500

    carrito compra

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//Variables
+const carrito = document.querySelector("#carrito");
+const listaCursos = document.querySelector("#lista-cursos");
+const ContenedorCarrito = document.querySelector("#lista-carrito");
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
