35f95f55529946fdb38859e5543e29d6530ebf53
Joel Guerrero <joelg1014@hotmail.com>
2025-11-23 12:35:26 -0500

    añadiendo al carrito

--- js/app.js
+++ js/app.js
@@ -1,5 +1,54 @@
 //Variables
 const carrito = document.querySelector("#carrito");
-const listaCursos = document.querySelector("#lista-cursos");
-const ContenedorCarrito = document.querySelector("#lista-carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
+const listaCursos = document.querySelector("#lista-cursos");
+let articulosCarrito = [];
+
+cargarEventListeners();
+function cargarEventListeners() {
+  listaCursos.addEventListener("click", agregarCurso);
+}
+
+//funciones
+function agregarCurso(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("agregar-carrito")) {
+    const cursoSeleccionar = e.target.parentElement.parentElement;
+    console.log();
+    leerDatosCursos(cursoSeleccionar);
+  }
+}
+
+// Leer el contenido del HTML al darle click
+function leerDatosCursos(curso) {
+  const infoCurso = {
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    precio: curso.querySelector(".precio span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+    cantidad: 1,
+  };
+
+  articulosCarrito = [...articulosCarrito, infoCurso];
+  console.log(articulosCarrito);
+  carritoHtml();
+}
+function carritoHtml() {
+  limpiarHTML();
+  articulosCarrito.forEach((curso) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>
+      ${curso.titulo}
+    </td>
+    `;
+    contenedorCarrito.appendChild(row);
+  });
+}
+
+function limpiarHTML() {
+  while (contenedorCarrito.firstChild) {
+    contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+  }
+}
