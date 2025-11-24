82a13915e16b1c60c541ad8d1c4537f529883af9
Joel Guerrero <joelg1014@hotmail.com>
2025-11-23 23:10:33 -0500

    terminado carrito de curso

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ let articulosCarrito = [];
 cargarEventListeners();
 function cargarEventListeners() {
   listaCursos.addEventListener("click", agregarCurso);
+  carrito.addEventListener("click", eliminarCurso);
+  vaciarCarritoBtn.addEventListener("click", vaciarCarrito);
 }
 
 //funciones
@@ -17,6 +19,19 @@ function agregarCurso(e) {
     const cursoSeleccionar = e.target.parentElement.parentElement;
     console.log();
     leerDatosCursos(cursoSeleccionar);
+  }
+}
+
+function vaciarCarrito() {
+  articulosCarrito = [];
+  limpiarHTML();
+}
+
+function eliminarCurso(e) {
+  if (e.target.classList.contains("borrar-curso")) {
+    const cursoID = e.target.getAttribute("data-id");
+    articulosCarrito = articulosCarrito.filter((curso) => curso.id !== cursoID);
+    carritoHtml();
   }
 }
 
@@ -30,17 +45,35 @@ function leerDatosCursos(curso) {
     cantidad: 1,
   };
 
-  articulosCarrito = [...articulosCarrito, infoCurso];
-  console.log(articulosCarrito);
+  const existe = articulosCarrito.some((curso) => curso.id === infoCurso.id);
+  if (existe) {
+    const cursos = articulosCarrito.map((curso) => {
+      if (curso.id === infoCurso.id) {
+        curso.cantidad++;
+        return curso;
+      } else {
+        return curso;
+      }
+    });
+    articulosCarrito = [...cursos];
+  } else {
+    articulosCarrito = [...articulosCarrito, infoCurso];
+  }
+
   carritoHtml();
 }
 function carritoHtml() {
   limpiarHTML();
   articulosCarrito.forEach((curso) => {
+    const { imagen, titulo, precio, cantidad, id } = curso;
     const row = document.createElement("tr");
     row.innerHTML = `
+    <td><img src="${imagen}" width="100"></td>
+    <td>${titulo}</td>
+    <td>${precio}</td>
+    <td>${cantidad}</td>
     <td>
-      ${curso.titulo}
+      <a href="#" class="borrar-curso" data-id="${id}">X</a>
     </td>
     `;
     contenedorCarrito.appendChild(row);
